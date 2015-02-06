d5cc744e140902c9393b05e4f3be90d59aa04048
bryantidd <bntidd@gmail.com>
2015-02-06 14:26:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 const Name = "consul-template"
 
 // Version is the current version of this application.
-const Version = "0.6.6.dev"
+const Version = "0.6.6.bnt"
 
 func main() {
 	cli := NewCLI(os.Stdout, os.Stderr)
